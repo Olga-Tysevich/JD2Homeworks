a8439ca30c7a950f15ffb5b0be51953b40e98f2e
Olga Tysevich <Olga.Tisevich@gmail.com>
2024-01-12 15:44:51 +0300

    updated project

--- src/main/java/org/example/lesson2/task1scientists/CompetitionDemo.java
+++
@@ -1,8 +0,0 @@
-package org.example.lesson2.task1scientists;
-
-public class CompetitionDemo {
-    public static void main(String[] args) {
-        Competition competition = new Competition();
-        competition.startCompetition();
-    }
-}
