7181f26d25b46fba34df7502d0e0abde5c08f97d
Olga Tysevich <Olga.Tisevich@gmail.com>
2024-01-16 00:04:40 +0300

    updated lesson2 v2

--- src/main/java/org/example/lesson2/task1scientists/models/Factory.java
+++ src/main/java/org/example/lesson2/task1scientists/models/Factory.java
@@ -9,8 +9,7 @@ import java.util.Random;
 import static org.example.lesson2.task1scientists.Constants.*;
 
 public class Factory extends Thread {
-    private Competition competition;
-    private Random random = new Random();
+    private final Competition competition;
 
     public Factory(Competition competition) {
         this.competition = competition;
@@ -20,7 +19,7 @@ public class Factory extends Thread {
     public void run() {
         for (int i = 1; i <= NUMBER_OF_NIGHTS; i++) {
             List<RobotParts> parts = createRandomParts(
-                    random.nextInt(FACTORY_MAX_NUMBER_OF_PARTS + 1
+                    RANDOM.nextInt(FACTORY_MAX_NUMBER_OF_PARTS + 1
                             - FACTORY_MIN_NUMBER_OF_PARTS)
                             + FACTORY_MIN_NUMBER_OF_PARTS);
             competition.putParts(parts);
@@ -32,7 +31,7 @@ public class Factory extends Thread {
     public List<RobotParts> createRandomParts(int numberOfParts) {
         List<RobotParts> parts = new ArrayList<>();
         for (int i = 0; i < numberOfParts; i++) {
-            parts.add(RobotParts.values()[random.nextInt(RobotParts.values().length)]);
+            parts.add(RobotParts.values()[RANDOM.nextInt(RobotParts.values().length)]);
         }
         return parts;
     }
