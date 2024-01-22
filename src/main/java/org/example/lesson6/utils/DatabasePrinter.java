2a4ea4899d4c14629949a1aedff8f28569d88798
Olga Tysevich <Olga.Tisevich@gmail.com>
2024-01-22 17:21:35 +0300

    fix homework lesson 6

--- src/main/java/org/example/lesson6/utils/DatabasePrinter.java
+++ src/main/java/org/example/lesson6/utils/DatabasePrinter.java
@@ -6,14 +6,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DatabasePrinter {
+    private static final String SCHEMAS_MESSAGE = "Schemas:";
+    private static final String CATALOG_MESSAGE = "%d) %s\n";
+    private static final String TABLE_MESSAGE = "Table: %s.%s\n";
+    private static final String COLUMN_MESSAGE = "Column: name: %s, type: %s, size: %s, default value: %s\n";
+    private static final String COLUMN_NAME_KEY = "COLUMN_NAME";
+    private static final String COLUMN_TYPE_KEY = "TYPE_NAME";
+    private static final String COLUMN_SIZE_KEY = "COLUMN_SIZE";
+    private static final String COLUMN_DEFAULT_KEY = "COLUMN_DEF";
 
     public static void printAllSchemas() {
         try {
             ResultSet resultCatalogs = SQLConnector.getConnection().getMetaData().getCatalogs();
-            System.out.println("Schemas:");
+            System.out.println(SCHEMAS_MESSAGE);
             int count = 1;
             while (resultCatalogs.next()) {
-                System.out.println(count++ + ") " + resultCatalogs.getString(1));
+                System.out.printf(CATALOG_MESSAGE, count++, resultCatalogs.getString(1));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -25,12 +33,13 @@ public class DatabasePrinter {
             ResultSet resultSet = SQLConnector.getConnection().getMetaData().getColumns(databaseName, null,
                     tableName, null);
 
-            System.out.println("Table: " + databaseName + "." + tableName);
+            System.out.printf(TABLE_MESSAGE, databaseName, tableName);
             while (resultSet.next()) {
-                System.out.println("Column: name: " + resultSet.getString("COLUMN_NAME")
-                        + ", type: " + resultSet.getString("TYPE_NAME")
-                        + ", size: " + resultSet.getString("COLUMN_SIZE")
-                        + ", default value: " + resultSet.getString("COLUMN_DEF"));
+                System.out.printf(COLUMN_MESSAGE,
+                        resultSet.getString(COLUMN_NAME_KEY),
+                        resultSet.getString(COLUMN_TYPE_KEY),
+                        resultSet.getString(COLUMN_SIZE_KEY),
+                        resultSet.getString(COLUMN_DEFAULT_KEY));
             }
         } catch (SQLException e) {
             e.printStackTrace();
