c40b64e60df5b96896ab04b6effda4d13a77fed4
Olga Tysevich <Olga.Tisevich@gmail.com>
2024-02-23 14:32:25 +0300

    updated dao

--- src/main/java/org/example/lesson9/dao/impl/PersonDAOImpl.java
+++ src/main/java/org/example/lesson9/dao/impl/PersonDAOImpl.java
@@ -3,9 +3,10 @@ package org.example.lesson9.dao.impl;
 import org.example.lesson9.dao.PersonDAO;
 import org.example.lesson9.dto.PersonDTO;
 
+import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
-import java.util.function.Supplier;
+import java.util.function.Consumer;
 
 import static org.example.lesson9.utils.Constants.*;
 
@@ -13,18 +14,19 @@ public class PersonDAOImpl extends DAOImpl<PersonDTO> implements PersonDAO {
 
     @Override
     public PersonDTO increaseAge(int id, int increment) {
-        Supplier<PersonDTO> increaseAge = () -> {
-            Query query = createNativeQuery(INCREASE_AGE, PersonDTO.class);
-            query.setParameter(INCREMENT, increment)
-                    .setParameter(ID, id);
-            query.executeUpdate();
-            PersonDTO result = get(id);
-            if (result != null) {
-                refreshObject(result);
-            }
-            return result;
-        };
-        return executeTransaction(increaseAge);
+        PersonDTO result = get(id);
+        if (result != null) {
+            EntityManager manager = getManager();
+            Consumer<PersonDTO> increaseAge = (o) -> {
+                Query query = manager.createNativeQuery(INCREASE_AGE, PersonDTO.class);
+                query.setParameter(INCREMENT, increment)
+                        .setParameter(ID, id);
+                query.executeUpdate();
+            };
+            executeTransaction(increaseAge, result);
+            manager.refresh(result);
+        }
+        return result;
     }
 
     @Override
